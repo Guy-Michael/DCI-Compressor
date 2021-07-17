996fc493f6720ca174b0759b785eccc1bec8084a
Guy Rahamim <guy.danino5@gmail.com>
2021-07-17 14:59:11 +0300

    Initial commit

---
+++ DCICompressor/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+class h
+{
+	public static void Main(String[] args)
+	{
+		int[] array = { 1, 2, 3, 4, 5, 6, 1, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+		int target = 7;
+		Console.WriteLine(binarySearch(array, target));
+	}
+	public static int binarySearch(int[] array, int target)
+	{
+		int lp = 0, rp = array.Length - 1;
+
+		while (lp != rp)
+		{
+			int currentIndex = (lp + rp) / 2;
+			if (array[currentIndex] == target)
+			{
+				return currentIndex;
+			}
+
+			else if (array[currentIndex] > target)
+			{
+				rp = currentIndex;
+			}
+
+			else lp = currentIndex;
+
+			Console.WriteLine(lp + "\t" + rp);
+			Thread.Sleep(1000);
+		}
+
+
+		return -1;
+	}
+}
---
+++ DCICompressor/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DCICompressor
+{
+	class Program
+	{
+		public static List<Tuple<String, float>> list = new List<Tuple<String, float>>();
+		static float[] initialScale = { 0, 0.6f, 0.8f, 1 };
+
+
+		static void Main(string[] args)
+		{
+			//word is ABBCA
+			list.Add(new Tuple<String, float>("A", 0.6f));
+			list.Add(new Tuple<String, float>("B", 0.2f));
+			list.Add(new Tuple<String, float>("C", 0.2f));
+			float[] scale = new float[4];
+			scale[0] = 0;
+			scale[1] = 0.6f;
+			scale[2] = 0.8f;
+			scale[3] = 1;
+
+			//letter A
+			scale = rescaleArray(scale, 0, 1);
+			//letter B
+			scale = rescaleArray(scale, 1, 2);
+
+			////letter B
+			scale = rescaleArray(scale, 1, 2);
+
+			////letter C
+			scale = rescaleArray(scale, 2, 3);
+
+			////letter A
+			scale = rescaleArray(scale, 0, 1);
+
+
+			//	//Part two.
+			//	float[] scaleForCompression = { scale[0], scale[1] };
+			//	float[] alternatingVal = { 0, 1 };
+			//	string compressedMessage = "";
+
+
+		}
+
+
+		public static string encode(float[] array, int minIndex, int maxIndex)
+		{
+			string code = "";
+
+			float minVal = array[minIndex], maxVal = array[maxIndex];
+			float lp = 0, rp = 1;
+			while (!(lp>minVal && lp<maxVal && rp>minVal && rp<maxVal))
+			{
+
+			}
+
+
+
+
+
+			return code;
+		}
+			public static float[] rescaleArray(float[] array,int minIndex,int maxIndex)
+		{
+			float delta = (array[maxIndex] - array[minIndex]);
+			Console.WriteLine("delta: " + delta + "\t min: " + array[minIndex] + "\tmax: " + array[maxIndex]);
+			float[] rescaledArray = new float[array.Length];
+
+			rescaledArray[0] = array[minIndex];
+			for (int i=1; i<rescaledArray.Length; i++)
+			{
+				rescaledArray[i] = delta * initialScale[i] + rescaledArray[0];
+			}
+
+			for (int i = 0; i < rescaledArray.Length; i++)
+			{
+				Console.WriteLine(rescaledArray[i]);
+			}
+
+			Console.WriteLine();
+			return rescaledArray;
+		}
+	}
+}
