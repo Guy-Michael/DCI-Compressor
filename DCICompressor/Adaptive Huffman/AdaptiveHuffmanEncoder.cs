04701c70014c1b7aa1c2a5fdae1155d7ade7b87c
Guy Rahamim <guy.danino5@gmail.com>
2021-08-13 23:51:21 +0300

    Added HuffNode class.

--- DCICompressor/Adaptive Huffman/AdaptiveHuffmanEncoder.cs
+++ DCICompressor/Adaptive Huffman/AdaptiveHuffmanEncoder.cs
@@ -5,6 +5,12 @@ namespace DCICompressor.Adaptive_Huffman
 {
 	class AdaptiveHuffmanEncoder
 	{
+		public static void Encode(string pathToFileToEncode, string pathToDestination)
+		{
+			BMPFile file = new BMPFile(pathToFileToEncode);
+		}
+
+		private static 
 
 	}
 }
---
+++ DCICompressor/Adaptive Huffman/HuffNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCICompressor.Adaptive_Huffman
+{
+	class HuffNode<T>
+	{
+		private HuffNode<T> m_LeftChild;
+		private HuffNode<T> m_RightChild;
+		private T m_Value;
+		private int m_Frequency;
+		public HuffNode<T> LeftChild
+		{
+			get { return m_LeftChild; }
+			set { m_LeftChild = value; }
+		}
+
+		public HuffNode<T> RightChild
+		{
+			get { return m_RightChild; }
+			set { m_RightChild = value; }
+		}
+
+		public T Value
+		{
+			get { return m_Value; }
+			set { m_Value = value; }
+		}
+
+		public int Frequency
+		{
+			get { return m_Frequency; }
+			set { m_Frequency = value; }
+		}
+
+		public bool IsLeaf()
+		{
+			return (LeftChild == null && RightChild == null);
+		}
+
+	}
+}
